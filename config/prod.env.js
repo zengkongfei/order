84687b63371a0c8d3032086b0d82a1ca57e331f4
zzf <373434301@qq.com>
2019-02-20 15:12:13 +0800

    新增多环境打包配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,23 @@
 'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.30.190:4011/"',
+
+const tagert = process.env.npm_lifecycle_event
+process.stdout.write('the env config file is '+ tagert +'\n')
+let base = {}
+if (tagert === 'test') {
+  base = {
+    NODE_ENV: '"production"',
+    BASE_API: '"http://192.168.30.230:4011/"'
+  }
+}else if (tagert === 'build') {
+  base = {
+    NODE_ENV: '"production"',
+    BASE_API: '"http://47.111.99.118:4011/"'
+  }
+}else if (tagert === 'joint') {
+  base = {
+    NODE_ENV: '"production"',
+    BASE_API: '"http://192.168.30.190:4011/"'
+  }
 }
+
+module.exports = base
