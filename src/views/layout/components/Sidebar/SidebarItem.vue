349efba169ce40f56ec9d5357d7ef1b06bc56444
heqiong <2859951752@qq.com>
2019-03-04 16:35:23 +0800

    添加侧边栏 小圆点

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,17 +1,18 @@
 <template>
   <div v-if="!item.hidden&&item.children" class="menu-wrapper">
 
-    <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+    <template
+      v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item v-if="onlyOneChild" :icon="onlyOneChild.icon||item.icon" :title="onlyOneChild.name" />
+          <item v-if="onlyOneChild" :icon="onlyOneChild.icon||item.icon" :title="onlyOneChild.name"/>
         </el-menu-item>
       </app-link>
     </template>
 
     <el-submenu v-else :index="resolvePath(item.path)">
       <template slot="title">
-        <item v-if="item.name" :icon="item.icon" :title="item.name" />
+        <item v-if="item.name" :icon="item.icon" :title="item.name"/>
       </template>
 
       <template v-for="child in item.children" v-if="!child.hidden">
@@ -21,10 +22,11 @@
           :item="child"
           :key="child.path"
           :base-path="resolvePath(child.path)"
-          class="nest-menu" />
+          class="nest-menu"/>
         <app-link v-else :to="resolvePath(child.path)" :key="child.name">
           <el-menu-item :index="resolvePath(child.path)">
-            <span class="dot-box"></span><item v-if="child" :icon="child.icon" :title="child.name" />
+            <span class="dot-box"></span>
+            <item v-if="child" :icon="child.icon" :title="child.name"/>
           </el-menu-item>
         </app-link>
       </template>
@@ -34,106 +36,145 @@
 </template>
 
 <script>
-import path from 'path'
-import { isExternal } from '@/utils'
-import Item from './Item'
-import AppLink from './Link'
+  import path from 'path'
+  import {isExternal} from '@/utils'
+  import Item from './Item'
+  import AppLink from './Link'
 
-export default {
-  name: 'SidebarItem',
-  components: { Item, AppLink },
-  props: {
-    // route object
-    item: {
-      type: Object,
-      required: true
+  export default {
+    name: 'SidebarItem',
+    components: {Item, AppLink},
+    props: {
+      // route object
+      item: {
+        type: Object,
+        required: true
+      },
+      isNest: {
+        type: Boolean,
+        default: false
+      },
+      basePath: {
+        type: String,
+        default: ''
+      }
     },
-    isNest: {
-      type: Boolean,
-      default: false
+    data() {
+      return {
+        onlyOneChild: null
+      }
     },
-    basePath: {
-      type: String,
-      default: ''
-    }
-  },
-  data() {
-    return {
-      onlyOneChild: null
-    }
-  },
-  methods: {
-    hasOneShowingChild(children, parent) {
-      const showingChildren = children.filter(item => {
-        if (item.hidden) {
-          return false
-        } else {
-          // Temp set(will be used if only has one showing child)
-          this.onlyOneChild = item
+    methods: {
+      hasOneShowingChild(children, parent) {
+        const showingChildren = children.filter(item => {
+          if (item.hidden) {
+            return false
+          } else {
+            // Temp set(will be used if only has one showing child)
+            this.onlyOneChild = item
+            return true
+          }
+        })
+
+        // When there is only one child router, the child router is displayed by default
+        if (showingChildren.length === 1) {
           return true
         }
-      })
 
-      // When there is only one child router, the child router is displayed by default
-      if (showingChildren.length === 1) {
-        return true
-      }
+        // Show parent if there are no child router to display
+        if (showingChildren.length === 0) {
+          this.onlyOneChild = {...parent, path: '', noShowingChildren: true}
+          return true
+        }
 
-      // Show parent if there are no child router to display
-      if (showingChildren.length === 0) {
-        this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
-        return true
+        return false
+      },
+      resolvePath(routePath) {
+        if (this.isExternalLink(routePath)) {
+          return routePath
+        }
+        return path.resolve(this.basePath, routePath)
+      },
+      isExternalLink(routePath) {
+        return isExternal(routePath)
       }
-
-      return false
-    },
-    resolvePath(routePath) {
-      if (this.isExternalLink(routePath)) {
-        return routePath
-      }
-      return path.resolve(this.basePath, routePath)
-    },
-    isExternalLink(routePath) {
-      return isExternal(routePath)
     }
   }
-}
 </script>
+<style>
+  .menu-wrapper .dot-box {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    background: white;
+    border-radius: 4px;
+    margin-right:7px;
+  }
+</style>
 <style scoped>
-  .menu-wrapper{ background: #262A37;}
+  .menu-wrapper {
+    background: #262A37;
+  }
 
   #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title, #app .sidebar-container .el-submenu .el-menu-item {
     min-width: 230px !important;
-     color: #FFF;
-    height: 50px; line-height: 50px;
+    color: #FFF;
+    height: 50px;
+    line-height: 50px;
     font-size: 16px !important;
   }
+
   .el-menu-item * {
     vertical-align: middle;
     color: #FFF;
     font-size: 16px;
   }
-  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title, #app .sidebar-container .el-submenu .el-menu-item:focus{
+
+  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title, #app .sidebar-container .el-submenu .el-menu-item:focus {
     background-color: #0082F0 !important
   }
 
-  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title, #app .sidebar-container .el-submenu .el-menu-item:link{
+  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title, #app .sidebar-container .el-submenu .el-menu-item:link {
     background-color: #0082F0 !important;
   }
-  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title[data-v-3ece4f7e], #app .sidebar-container .el-submenu .el-menu-item{ text-align: center;}
-  .active{ background-color: #0082F0!important;width: 100%;
+
+  #app .sidebar-container .nest-menu .el-submenu > .el-submenu__title[data-v-3ece4f7e], #app .sidebar-container .el-submenu .el-menu-item {
+    text-align: center;
+  }
+
+  .active {
+    background-color: #0082F0 !important;
+    width: 100%;
     display: block;
     height: 50px;
-    line-height: 50px;}
-    .el-submenu__title span{
-      font-size: 16px !important;
-    }
+    line-height: 50px;
+  }
+
+  .el-submenu__title span {
+    font-size: 16px !important;
+  }
 </style>
 <style>
-  #app .sidebar-container{width: 230px !important;}
-  #app .main-container{margin-left: 230px;}
-  .el-submenu__title{ background:rgba(49,67,88,1);}
-  .el-submenu__title:hover{background-color:#3E5E84!important;}
-  #app .el-menu-item, .el-submenu__title{height: 60px; line-height: 60px;background: #314358!important;}
+  #app .sidebar-container {
+    width: 230px !important;
+  }
+
+  #app .main-container {
+    margin-left: 230px;
+  }
+
+  .el-submenu__title {
+    background: rgba(49, 67, 88, 1);
+  }
+
+  .el-submenu__title:hover {
+    background-color: #3E5E84 !important;
+  }
+
+  #app .el-menu-item, .el-submenu__title {
+    height: 60px;
+    line-height: 60px;
+    background: #314358 !important;
+  }
 
 </style>
