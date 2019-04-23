4ffe8227b9262dc620f4ff5550a3b55eb78193fe
xuhuiqing <673880450@qq.com>
2019-04-23 19:43:29 +0800

    加路由

--- src/router/index.js
+++ src/router/index.js
@@ -57,13 +57,58 @@ export const constantRouterMap = [
       path: '/memberMag/memberInfo',
       name: '会员信息',
       component: () => import('@/views/memberMag/memberInfo')
-    }
+    },
     // {
     //   path: '/memberMag/generalize',
     //   name: '网点推广',
     //   component: () => import('@/views/memberMag/generalize')
     // }
     ]
+  },
+  {
+    path: '/indent',
+    component: Layout,
+    redirect: '/indent/indentlist',
+    name: '订单中心',
+    hidden: true,
+    // icon: 'banben',
+    children: [{
+      path: '/indent/indentlist',
+      hidden: true,
+      name: '订单管理',
+      component: () => import('@/views/indent/indentlist')
+    },
+    {
+      path: '/indent/addindent',
+      name: '订单新增',
+      hidden: true,
+      component: () => import('@/views/indent/addindent')
+    },
+    {
+      path: '/indent/editindent',
+      name: '订单修改',
+      hidden: true,
+      component: () => import('@/views/indent/editindent')
+    },
+    // {
+    //   path: '/salesreturn/salesreturnlist',
+    //   hidden: true,
+    //   name: '退货单管理',
+    //   component: () => import('@/views/salesreturn/salesreturnlist')
+    // },
+    // {
+    //   path: '/salesreturn/addsalesreturn',
+    //   hidden: true,
+    //   name: '新增退货单',
+    //   component: () => import('@/views/salesreturn/addsalesreturn')
+    // },
+    // {
+    //   path: '/salesreturn/editsalesreturn',
+    //   hidden: true,
+    //   name: '退货单管理',
+    //   component: () => import('@/views/salesreturn/editsalesreturn')
+    // }
+  ]
   },
 
   { path: '*', redirect: '/404', hidden: true }
